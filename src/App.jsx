9d91e14f5d803f12c7033a044033704b267051c3
Elena` <go.lena.83@gmail.com`>
2024-02-22 12:20:33 +0200

    com

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React, { useRef } from 'react';
+import React from 'react';
+import logo from './assets/loggo.jpg'; 
 
 
 import "./App.css"
 
+
+
+
 export const App = () => {
     return (
-    <div>
-      
-    </div>
+      <div className='box'>
+        <img src={logo} alt="Logo" />
+        Hello World
+      </div>
   );
 };
